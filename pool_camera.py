7c28539bf8acb93197f82d19997ccdd0b7c9adc1
Vicky McDermott <vickymmcd@gmail.com>
2017-04-23 16:08:47 -0400

    added contour detection

--- pool_camera.py
+++ pool_camera.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Code to make the pool camera work with the submarine for ORCAS.
 Spring 2017
@@ -8,9 +9,10 @@ import cv2
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import time
 
 class PoolCam:
-    def __init__():
+    def __init__(self):
         '''
         Initializes the PoolCam object to be used
         to gather raw camera data that can be sent to
@@ -44,25 +46,59 @@ class PoolCam:
         except CvBridgeError as e:
             print(e)
 
-    def capture_images(self):
+    def capture_images(self, image):
         '''
         Captures and displays raw camera feed and also
-        displays the feed with a gaussian blur filter applied.
+        displays the feed.
         '''
 
+        if image == None:
+            return
+
+        # Feed Display(s):
+        cv2.imshow('Raw Feed (feed)',image)
+        # would like to understand why this makes video stream
+        k = cv2.waitKey(5) & 0xFF
+
+    def apply_filter(self, image):
+        '''
+        '''
+        if image == None:
+            return
+
+        blur = cv2.GaussianBlur(image, (5,5), 0) # Gaussian Blur filter
+        #cv2.imshow('Gaussian Blur', blur)
+        #k = cv2.waitKey(5) & 0xFF
+        # Changes BGR video to GRAY and threshold it
+        vidgray = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
+        ret,thresh = cv2.threshold(vidgray,200,255,cv2.THRESH_BINARY_INV)
+        return thresh
+
+    def contour_feed(self, feed, thresh):
+        '''
+        '''
         if self.cv_image == None:
             return
 
-        # Gaussian Blur filter
-        blur = cv2.GaussianBlur(self.cv_image, (5,5), 0) # Gaussian Blur filter
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-        # Feed Display(s):
-        cv2.imshow('Raw Feed (feed)',self.cv_image)
-        cv2.imshow('Gaussian Blur Filter (blur)', self.cv_image)
+        sub_contours = [contour for contour in contours if cv2.contourArea(contour) > 200 and cv2.contourArea(contour) < 500000]
+
+        cv2.drawContours(feed, sub_contours, -1, (0,255,0), 3)
+
+
+    def run(self):
+        r = rospy.Rate(20) # Sets update rate
+        while not rospy.is_shutdown():
+            r.sleep()
+            # if self.debug == True: self.calibrate()
+            self.capture_images(self.cv_image)
+            vid = self.apply_filter(self.cv_image)
+            self.contour_feed(self.cv_image, vid)
 
 if __name__=='__main__':
     '''
     Test the camera
     '''
     pc = PoolCam() # Creates new instance of class PoolCam
-    pc.capture_images() # Calls run function
+    pc.run() # Calls run function
