b6db5dc4ead1de89ae35aec1cf1fffc560248c89
Mark Goldwater <mark.goldwater@students.olin.edu>
2017-11-05 15:35:12 -0500

    working radio controller

--- src/tests/RadioTest/RadioTest.ino
+++ src/tests/RadioTest/RadioTest.ino
@@ -1,6 +1,6 @@
 #include <EnableInterrupt.h>
 
-#define SERIAL_PORT_SPEED 57600
+#define SERIAL_PORT_SPEED 9600
 #define RC_NUM_CHANNELS  4
 
 #define RC_CH1  0
@@ -13,9 +13,14 @@
 #define RC_CH3_INPUT  A2
 #define RC_CH4_INPUT  A3
 
+
 uint16_t rc_values[RC_NUM_CHANNELS];
 uint32_t rc_start[RC_NUM_CHANNELS];
 volatile uint16_t rc_shared[RC_NUM_CHANNELS];
+
+const int motorPin = 9;//attaches the motor to pin 9
+int inputValue = 0;//variable to store the value coming from transmitter
+int outputValue = 0;//variable to store the remapped output value
 
 void rc_read_values() {
   noInterrupts();
@@ -53,11 +58,30 @@ void setup() {
 
 void loop() {
   rc_read_values();
-
-  Serial.print("CH1:"); Serial.print(rc_values[RC_CH1]); Serial.print("\t");
-  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
-  Serial.print("CH3:"); Serial.print(rc_values[RC_CH3]); Serial.print("\t");
-  Serial.print("CH4:"); Serial.println(rc_values[RC_CH4]);
-
+  
+  inputValue = rc_values[RC_CH1];//read the value from the transmitter
+  
+  if (map(inputValue,1090,1900,0,255) < 0)
+  {
+    outputValue = 0;
+  }
+  else if (map(inputValue,1090,1900,0,255) > 255)
+  {
+    outputValue = 255;
+  }
+  else
+  {
+    outputValue = map(inputValue,1090,1900,0,255);
+  }                                                 ;//Convert from 0-1023 proportional to the number of a number of from 0 to 255
+  
+  analogWrite(motorPin,outputValue);//turn the motor on depending on the output value
+  
+  //Serial.print("CH1:"); Serial.print(rc_values[RC_CH1]); Serial.print("\t");
+  //Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
+  //Serial.print("CH3:"); Serial.print(rc_values[RC_CH3]); Serial.print("\t");
+  //Serial.print("CH4:"); Serial.println(rc_values[RC_CH4]);
+  
+  Serial.println(outputValue);
+  
   delay(200);
 }
