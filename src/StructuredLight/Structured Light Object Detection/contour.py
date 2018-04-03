42c93862a9b4e14a1748f6fc79f0c66fa515a2f2
Danny Wolf <wolf@students.olin.edu>
2018-04-03 15:23:17 -0400

    Prototype calibration

--- src/StructuredLight/Structured Light Object Detection/contour.py
+++ src/StructuredLight/Structured Light Object Detection/contour.py
@@ -1,10 +1,38 @@
 import cv2
 import numpy as np
 import math
+import scipy
 
 DEBUG = False
 
-def find_contours(img):
+def calibrate_laser(mask, distance):
+    rows, cols = mask.shape
+    center_of_masses = np.array(cols)
+    for col in range(cols):
+        column = mask[:, col]
+        if any(column):
+            center_of_masses[col] = scipy.ndimage.measurements.center_of_mass(column)[0]
+
+    mean_laser_px = np.mean(
+        center_of_masses[np.nonzero(center_of_masses)]
+    )
+
+    # TODO(danny): figure out relationship between px height and distance relative to unknown center
+    return mean_laser_px
+
+def get_contours(img, mask):
+    # contours processing
+    (_, contours, _) = cv2.findContours(mask.copy(), cv2.RETR_LIST, 1)
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area < 8: continue
+        epsilon = 0.1 * cv2.arcLength(c, True) # tricky smoothing to a single line
+        approx = cv2.approxPolyDP(c, epsilon, True)
+        cv2.drawContours(img, [approx], -1, [255, 255, 255], -1)
+    return img
+
+
+def find_laser(img, find_contours=False):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     RED_LOW = np.array([0, 0, 0])
@@ -15,16 +43,7 @@ def find_contours(img):
         result = cv2.bitwise_and(img, img, mask = mask)
         return result
 
-    # contours processing
-    (_, contours, _) = cv2.findContours(mask.copy(), cv2.RETR_LIST, 1)
-    for c in contours:
-        area = cv2.contourArea(c)
-        if area < 8: continue
-        epsilon = 0.1 * cv2.arcLength(c, True) # tricky smoothing to a single line
-        approx = cv2.approxPolyDP(c, epsilon, True)
-        cv2.drawContours(img, [approx], -1, [255, 255, 255], -1)
-    
-    return img
+    return mask
 
 cap = cv2.VideoCapture(1)
 
@@ -32,9 +51,9 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    found_contours = find_contours(frame)
+    laser = find_laser(frame)
 
-    cv2.imshow('result', found_contours)
+    cv2.imshow('result', laser)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
