37eddd326067c7922088113fefc228c4b500d687
Emily Kohler <emilyrkohler@gmail.com>
2018-04-10 11:57:32 -0400

    now maps location to rudder position

--- src/StructuredLight/Structured Light Object Detection/pathPlan.py
+++ src/StructuredLight/Structured Light Object Detection/pathPlan.py
@@ -1,37 +1,43 @@
 # path plan in python
+import math
 
-def array_to_bool(occupancy, length):
-    threshold = 30
-    x = 0
-    bool_array = []
-    for x in range(0, length):
-        if occupancy[x] < threshold:
-            bool_array.append(False)
-        else:
-            bool_array.append(True)
-        x += 1
-    print bool_array
-    return bool_array
-
-def path_location (bool_array, length):
+def path_location (range_array):
     i = 0
     count = 0
-    result = 0
-    location = [0, 0]
+    longest_pos = 0
+    threshold = 40
+    location_range = [0, 0]
+    length = len(range_array)
     for i in range(0, length):
-        if bool_array[i] == False:
+        if range_array[i] <= threshold or math.isnan(range_array[i]):
             count = 0
         else:
             count += 1
-            if result < count:
-                result = count
-                location[0] = i-result+1
-                location[1] = i
+            if longest_pos < count:
+                longest_pos = count
+                location_range[0] = i-longest_pos+1
+                location_range[1] = i
         i += 1
-    print location
-    return location
+    center_point = (location_range[0]+location_range[1])/2.0
+    #print location_range
+    #print center_point
+    return [center_point, length]
 
+def rudder_pos(loc_array):
+    '''
+    normalized values:
+    r = 30, l = -30, s = 0
+    not normalized values:
+    r = 120, l = 60, s = 90
+    '''
+    location = loc_array[0]
+    length = loc_array[1]
+    slope = 30/(length/2.0)
+    intercept = -30.0
+    position = ((slope*location) + intercept) + 90.0
+    #print position
+    return int(position)
 
-length = 8
-test = [70, 60, 50, 10, 70, 60, 50, 40]
-path_location(array_to_bool(test, length), length)
+length = 10
+test = [70, 60, 50, 10, 70, 60, 50, 50, 50, 70]
+rudder_pos(path_location(test))
